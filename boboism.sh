27adec7a9746a72b7e56e7819e854f2adfdd7c08
Jianbo Su <sujianbo@gmail.com>
2013-08-08 17:32:12 +0800

    20130808

--- boboism.sh
+++ boboism.sh
@@ -3,6 +3,13 @@ if [ -f $HOME/.boboism/git/git-completion.bash ]; then
   source $HOME/.boboism/git/git-completion.bash
 fi
 if [ -f $HOME/.boboism/git/git-prompt.sh ]; then 
+  GIT_PS1_SHOWDIRTYSTATE=true 
+  GIT_PS1_SHOWSTASHSTATE=true
+  GIT_PS1_SHOWUNTRACKEDFILES=true
+  GIT_PS1_SHOWUPSTREAM=(verbose legacy git svn)
+  GIT_PS1_DESCRIBE_STYLE=branch
+  GIT_PS1_SHOWCOLORHINTS=true
   source $HOME/.boboism/git/git-prompt.sh
-  PS1='[\W$(__git_ps1 "{%s}")]\$ '
+  #PS1='[\W$(__git_ps1 "{%s}")]\$ '
+  PROMPT_COMMAND='__git_ps1 ["\W" "]\$ "'
 fi
