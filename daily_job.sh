aa2ad45a6b9138c392f31c20ac4d76b83fad1f70
Jianbo Su <sujianbo@gmail.com>
2014-07-28 18:52:09 +0800

    20140728185209

--- daily_job.sh
+++ daily_job.sh
@@ -1,6 +1,8 @@
 # check out git config from https://github.com/git
+
+cd $HOME/.boboism
+git pull
 wget --no-check-certificate https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -O $HOME/.boboism/git/git-completion.bash
 wget --no-check-certificate https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -O $HOME/.boboism/git/git-prompt.sh
-cd $HOME/.boboism
 git submodule init && git submodule update
 git submodule foreach git pull origin master
