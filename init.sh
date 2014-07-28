3f99423bc4ce175b50951962c45ae8416eab0374
Jianbo Su <sujianbo@gmail.com>
2014-07-28 07:27:47 -0700

    20140728072747

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
-ln -s ~/.boboism/ruby/gem/gemrc ~/.gemrc
-ln -s ~/.boboism/git/gitconfig ~/.gitconfig
-ln -s ~/.boboism/vim/gvimrc ~/.gvimrc
-ln -s ~/.boboism/ruby/jruby/jrubyrc ~/.jrubyrc
-ln -s ~/.boboism/ruby/rvm/rvmrc ~/.rvmrc
-ln -s ~/.boboism/tmux/tmux.conf ~/.tmux.conf
-ln -s ~/.boboism/vim ~/.vim
-ln -s ~/.boboism/vim/vimrc ~/.vimrc
-ln -s ~/.boboism/gem/gemrc ~/.gemrc
+ln -sf ~/.boboism/ruby/gem/gemrc ~/.gemrc
+ln -sf ~/.boboism/git/gitconfig ~/.gitconfig
+ln -sf ~/.boboism/vim/gvimrc ~/.gvimrc
+ln -sf ~/.boboism/ruby/jruby/jrubyrc ~/.jrubyrc
+ln -sf ~/.boboism/ruby/rvm/rvmrc ~/.rvmrc
+ln -sf ~/.boboism/tmux/tmux.conf ~/.tmux.conf
+ln -sf ~/.boboism/vim ~/.vim
+ln -sf ~/.boboism/vim/vimrc ~/.vimrc
+ln -sf ~/.boboism/gem/gemrc ~/.gemrc
 cd ~/.boboism
 git submodule init
 git submodule update
-./daily.sh
+./daily_job.sh
