af926a44a9dce0ae2d8679836d57c65abafcef63
Jianbo Su <sujianbo@gmail.com>
2014-07-28 18:53:12 +0800

    20140728185312

--- daily_job.sh
+++ daily_job.sh
@@ -6,3 +6,5 @@ wget --no-check-certificate https://raw.github.com/git/git/master/contrib/comple
 wget --no-check-certificate https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -O $HOME/.boboism/git/git-prompt.sh
 git submodule init && git submodule update
 git submodule foreach git pull origin master
+git add -A
+git commit -am "`date +%Y%m%d%H%M%S`"
